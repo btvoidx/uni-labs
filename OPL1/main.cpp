45c1b7d80a090c64044ab729fb3961fde316f72d
Max Tumozov <maksimtumozov@gmail.com>
2023-09-12 21:18:47 +0400

    fix input validation

--- OPL1/main.cpp
+++ OPL1/main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 void readTime(string prompt, int *hours, int *minutes, int *seconds) {
   while (
       ((cout << prompt) && scanf_s("%d:%d:%d", hours, minutes, seconds) != 3) ||
-      cin.peek() != '\n') {
+      cin.peek() != '\n' ||
+      (59 < *hours || *hours < 0 || 59 < *minutes || *minutes < 0 ||
+       59 < *seconds || *seconds < 0)) {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << "wrong time format: expected HH:MM:SS\n";
@@ -16,25 +18,20 @@ void readTime(string prompt, int *hours, int *minutes, int *seconds) {
 
 void run() {
   int startHours, startMinutes, startSeconds;
-  while (0 <= startHours && startHours <= 59 ||
-         0 <= startMinutes && startMinutes <= 59 ||
-         0 <= startSeconds && startSeconds <= 59)
-    readTime("enter start time: ", &startHours, &startMinutes, &startSeconds);
-  
   int endHours, endMinutes, endSeconds;
-  while (0 <= endHours && endHours <= 59 ||
-         0 <= endMinutes && endMinutes <= 59 ||
-         0 <= endSeconds && endSeconds <= 59)
-    readTime("enter end time: ", &endHours, &endMinutes, &endSeconds);
+  readTime("enter start time: ", &startHours, &startMinutes, &startSeconds);
+  readTime("enter end time: ", &endHours, &endMinutes, &endSeconds);
 
   int days = 0;
   int startTime = startHours * 3600 + startMinutes * 60 + startSeconds;
   int endTime = endHours * 3600 + endMinutes * 60 + endSeconds;
 
   if (startTime > endTime) {
-    cout << "ended before starting: assuming task took multiple days\n";
-    cout << "enter day count: ";
-    cin >> days;
+    cout << "task ended before starting: assuming task took multiple days\n";
+    while (days <= 0) {
+      cout << "enter day count (>0): ";
+      cin >> days;
+    }
   }
 
   int totalTimeSpent = endTime - startTime + days * 24 * 3600;

bcfec041f2327e38e8625c2258c9c05cb3c12519
Max Tumozov <maksimtumozov@gmail.com>
2023-09-28 11:59:26 +0400

    add changes made during class

--- OPL1/main.cpp
+++ OPL1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
@@ -7,15 +6,18 @@ void readTime(string prompt, int *hours, int *minutes, int *seconds) {
   while (
       // выполняем цикл, пока:
       // введено не ровно три числа через ':'
-      // ИЛИ одно из них не входит в интервал 0 <= x <= 59
+      // ИЛИ одно из них не входит в интервал 0 <= x <= 59 (23 для часов)
       ((cout << prompt) && scanf_s("%d:%d:%d", hours, minutes, seconds) != 3) ||
       cin.peek() != '\n' ||
-      (59 < *hours || *hours < 0 || 59 < *minutes || *minutes < 0 ||
+      (23 < *hours || *hours < 0 || 59 < *minutes || *minutes < 0 ||
        59 < *seconds || *seconds < 0)) {
-    // очищаем остатки ввода и пишем ошибку
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cout << "wrong time format: expected HH:MM:SS\n";
+    cin.clear(); // сбрасываем ошибку
+
+    // == cin.ignore(numeric_limits<streamsize>::max(), '\n')
+    while (cin.get() != '\n') {
+    };
+
+    cout << "wrong time format: expected HH:MM:SS\n\n";
   }
 }
 
@@ -35,9 +37,13 @@ void run() {
   // ведь машину времени не изобрели
   if (startTime > endTime) {
     cout << "task ended before starting: assuming task took multiple days\n";
-    while (days <= 0) {
-      cout << "enter day count (a positive integer): ";
-      cin >> days;
+    while (((cout << "enter day count: ") && !(cin >> days)) || days <= 0) {
+      cout << "wrong day count: expected a positive integer\n\n";
+      cin.clear(); // сбрасываем ошибку
+
+      // == cin.ignore(numeric_limits<streamsize>::max(), '\n')
+      while (cin.get() != '\n') {
+      };
     }
   }
 
@@ -59,6 +65,9 @@ void run() {
 }
 
 int main() {
+  cout << "this program calculates the difference between two time stamps\n";
+  cout << "expected time format: HH:MM:SS\n";
+
   string shouldRepeat;
   do {
     // настоящий "main" программы.
@@ -68,6 +77,8 @@ int main() {
 
     cout << "again? (y / anything else quits): ";
     cin >> shouldRepeat;
+    cout << "\n\n";
   } while (shouldRepeat == "y" || shouldRepeat == "Y");
+  
   return 0;
 }

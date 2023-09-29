09969f5b677b9a0c287fc52c54ad1bd6234c8e1b
Max Tumozov <maksimtumozov@gmail.com>
2023-09-29 21:02:34 +0400

    add OPL2

---
+++ OPL2/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double askDouble(string msg) {
+  double v;
+  while (!(cout << msg) || !(cin >> v) || cin.peek() != '\n') {
+    cout << "bad input\n";
+    cin.clear();
+    cin.ignore(LLONG_MAX, '\n');
+  }
+  return v;
+}
+
+int fact(int n) {
+  for (int i = n - 1; i > 1; i--) {
+    n *= i;
+  }
+  return n;
+}
+
+double f(double x, int n) {
+  return pow(-1, n) * (2 * n * pow(x, 2 * n + 1) / fact(2 * n + 1));
+}
+
+void run() {
+  double x = askDouble("enter x: ");
+  double a = askDouble("enter a: ");
+  while (a <= 0) {
+    cout << "a must be positive and non-zero\n";
+    a = askDouble("enter a (correctly): ");
+  }
+
+  double prev, curr, sum;
+
+  // если a целое
+  if (floor(a) == a) {
+    cout << "a is whole; calculating first " << a << " elements:\n";
+    for (int n = 1; n <= a; n++) {
+      prev = curr;
+      curr = f(x, n);
+      sum += curr;
+
+      printf("n: %d;\ta(n): %f\tsum: %f\te: %f\n", n, curr, sum,
+             abs(curr - prev));
+    }
+  } else {
+    cout << "a is real; calculating until e < " << a << "\n";
+    int n = 0;
+    do {
+      n += 1;
+
+      prev = curr;
+      curr = f(x, n);
+      sum += curr;
+
+      printf("n: %d;\ta(n): %f\tsum: %f\te: %f\n", n, curr, sum,
+             abs(curr - prev));
+
+    } while (abs(curr - prev) > a);
+  }
+}
+
+int main() {
+  cout << "this program calculates the scary formula given in the task\n";
+
+  string shouldRepeat;
+  do {
+    // настоящий "main" программы.
+    // он вынесен, чтобы отделить основную логику программы
+    // от вторичной логики перезапуска.
+    run();
+
+    cout << "again? (y / anything else quits): ";
+    cin >> shouldRepeat;
+    cout << "\n\n";
+  } while (shouldRepeat == "y" || shouldRepeat == "Y");
+
+  return 0;
+}

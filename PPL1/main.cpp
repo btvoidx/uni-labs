1cb6f90ee5dc60ff17c5b7e8c5045c8d4e3a2d96
Max Tumozov <maksimtumozov@gmail.com>
2023-10-02 16:10:45 +0400

    square matrix transform

--- PPL1/main.cpp
+++ PPL1/main.cpp
@@ -3,55 +3,31 @@
 
 using namespace std;
 
-int askInt(string msg) {
+int askInt(string msg, string errmsg) {
   cout << msg;
 
   int v;
   while (!(cin >> v) || cin.peek() != '\n') {
     cin.clear();
     cin.ignore(LLONG_MAX, '\n');
+    cout << errmsg;
     cout << msg;
   };
 
   return v;
 }
 
-string askString(string msg) {
-  cout << msg;
-
-  string v;
-  while (!(cin >> v) || cin.peek() != '\n') {
-    cin.clear();
-    cin.ignore(LLONG_MAX, '\n');
-    cout << msg;
-  };
-  return v;
+void transposeSquareMatrix(double **mat, int size) {
+  for (int a = 1; a < size; a++) {
+    for (int b = 0; b < a; b++) {
+      int tmp = mat[a][b];
+      mat[a][b] = mat[b][a];
+      mat[b][a] = tmp;
+    }
+  }
 }
 
-void run() {
-  // размеры матрицы
-  int w = 0, h = 0;
-  while (w < 1)
-    w = askInt("input matrix width (a positive number): ");
-  while (h < 1)
-    h = askInt("input matrix height (a positive number): ");
-
-  double **mat = new double *[h];
-  for (int hi = 0; hi < h; hi++)
-    mat[hi] = new double[w];
-
-  // наполняем матрицу
-  for (int hi = 0; hi < h; hi++)
-    for (int wi = 0; wi < w; wi++)
-      while (!printf_s("value at %d.%d: ", hi + 1, wi + 1) ||
-             !(cin >> mat[hi][wi]) || cin.peek() != '\n') {
-        cout << "sorry, numbers only\n";
-        cin.clear();
-        cin.ignore(LLONG_MAX, '\n');
-      }
-
-  // показываем изначальную матрицу
-  cout << "\ninput matrix:\n  ";
+void printMatrix(double **mat, int w, int h) {
   // min(x, 6) используется чтобы не выводить
   // слишком большие матрицы целиком
   for (int hi = 0; hi < min(h, 6); hi++) {
@@ -60,18 +36,39 @@ void run() {
     }
     cout << "\n  ";
   }
+}
 
-  // показываем транспонированную матрицу
+void run() {
+  // размеры матрицы
+  int size = 0;
+  while (size < 2)
+    size = askInt("input matrix size (a positive number): ",
+                  "sorry, numbers > 2 only\n");
+
+  double **mat = new double *[size];
+  for (int hi = 0; hi < size; hi++)
+    mat[hi] = new double[size];
+
+  // наполняем матрицу
+  for (int hi = 0; hi < size; hi++)
+    for (int wi = 0; wi < size; wi++)
+      while (!printf_s("value at %d.%d: ", hi + 1, wi + 1) ||
+             !(cin >> mat[hi][wi]) || cin.peek() != '\n') {
+        cout << "sorry, numbers only\n";
+        cin.clear();
+        cin.ignore(LLONG_MAX, '\n');
+      }
+
+  cout << "\ninput matrix:\n  ";
+  printMatrix(mat, size, size);
+
+  transposeSquareMatrix(mat, size);
+
   cout << "\ntransposed matrix:\n  ";
-  for (int wi = 0; wi < min(w, 6); wi++) {
-    for (int hi = 0; hi < min(h, 6); hi++) {
-      cout << mat[hi][wi] << "\t";
-    }
-    cout << "\n  ";
-  }
+  printMatrix(mat, size, size);
 
   // удаляем матрицу из памяти
-  for (int hi = 0; hi < h; hi++)
+  for (int hi = 0; hi < size; hi++)
     delete[] mat[hi];
   delete[] mat;
 }
